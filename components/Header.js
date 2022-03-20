ec2bc5f712487a2d51824c08abf38640b37dcd4a
VSCoder <vscoder.brad@gmail.com>
2022-03-20 14:42:00 +0800

    added nav

--- components/Header.js
+++ components/Header.js
@@ -8,17 +8,18 @@ import {
     HomeIcon
 } from '@heroicons/react/outline'
 import Image from 'next/image';
-import NavItem from './NavItem';
+import { useRouter } from 'next/router'
+import HeaderItem from './HeaderItem';
 const Header = () => {
     return (
         <header className='flex flex-col sm:flex-row m-5 justify-between items-center h-auto'>
             <div className='flex flex-grow justify-evenly max-w-2xl' >
-                <NavItem title='HOME' Icon={HomeIcon} />
-                <NavItem title='TRENDING' Icon={ LightningBoltIcon }  />
-                <NavItem title='VERIFIED' Icon={BadgeCheckIcon} />
-                <NavItem title='COLLECTIONS' Icon={CollectionIcon} />
-                <NavItem title='SEARCH' Icon={SearchIcon} />
-                <NavItem title='ACCOUNT' Icon={UserIcon} />
+                <HeaderItem title='HOME' Icon={HomeIcon} />
+                <HeaderItem title='TRENDING' Icon={ LightningBoltIcon }  />
+                <HeaderItem title='VERIFIED' Icon={BadgeCheckIcon} />
+                <HeaderItem title='COLLECTIONS' Icon={CollectionIcon} />
+                <HeaderItem title='SEARCH' Icon={SearchIcon} />
+                <HeaderItem title='ACCOUNT' Icon={UserIcon} />
             </div>
             <div >
                 <Image className='object-contain'
